b2ee63f4a654d30ca5c2a166e11a4c8c602759f5
revertan3 <no.esis323@gmail.com>
2021-09-23 14:06:15 +0900

    2DSpriteのinternalClassを用いて、SpriteのOutlineを拡張するためのテスト

---
+++ Assets/AutoOutlineGenerator/SpriteExtension/ShapeEditorExtension.cs
@@ -0,0 +1,14 @@
+using UnityEditor.U2D.Sprites;
+
+namespace AutoOutlineGenerator.Editor.SpriteExtension
+{
+    public class ShapeEditorExtension
+    {
+        private ShapeEditor s;
+
+        void Pre()
+        {
+            
+        }
+    }
+}
