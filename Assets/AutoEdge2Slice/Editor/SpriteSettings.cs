10731db883146b6dbbe2738505bad42815ac38b7
revertan3 <no.esis323@gmail.com>
2021-10-01 20:29:07 +0900

    SpriteSettingsを自動で生成する用に変更

--- Assets/AutoEdge2Slice/Editor/SpriteSettings.cs
+++ Assets/AutoEdge2Slice/Editor/SpriteSettings.cs
@@ -3,12 +3,24 @@ using UnityEngine;
 
 namespace AutoOutlineGenerator.Editor
 {
-    [CreateAssetMenu(fileName = "FILENAME", menuName = "SpriteSettings", order = 0)]
-    public class SpriteSettings : ScriptableObject
+    internal class SpriteSettings : ScriptableObject
     {
         public TextureImporterCompression _textureImporterCompression = TextureImporterCompression.Uncompressed;
         public FilterMode _filterMode;
         public float _pixelPerUnit;
         public int _maxTextureSize;
+
+        internal static SpriteSettings GetOrCreate()
+        {
+            var path = "Assets/AutoEdge2Slice/Editor/SpriteSettings.asset";
+            var settings = AssetDatabase.LoadAssetAtPath<SpriteSettings>(path);
+
+            if (settings != null) return settings;
+
+            settings = CreateInstance<SpriteSettings>();
+            AssetDatabase.CreateAsset(settings,path);
+            AssetDatabase.SaveAssets();
+            return settings;
+        } 
     }
 }
