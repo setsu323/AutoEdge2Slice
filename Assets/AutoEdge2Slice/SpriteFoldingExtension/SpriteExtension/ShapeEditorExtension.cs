ea59dc513b0afb6671e4c702d5c73ddfcf6ac876
revertan3 <no.esis323@gmail.com>
2022-08-26 10:44:54 +0900

    必要のない名前空間を削除

--- Assets/AutoEdge2Slice/SpriteFoldingExtension/SpriteExtension/ShapeEditorExtension.cs
+++ Assets/AutoEdge2Slice/SpriteFoldingExtension/SpriteExtension/ShapeEditorExtension.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.U2D.Sprites;
-using UnityEditor.UIElements;
 using UnityEngine;
 using SpriteUtility = UnityEditor.Sprites.SpriteUtility;
 
