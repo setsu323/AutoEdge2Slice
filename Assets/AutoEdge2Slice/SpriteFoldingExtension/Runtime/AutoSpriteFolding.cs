1ba49bf28f948dbe8b4b18b522044cd9f7c7335f
Revertan3 <32428760+setsu323@users.noreply.github.com>
2023-10-18 02:38:35 +0900

    AutoSpriteFoldingを分かりやすいように縮めた

--- Assets/AutoEdge2Slice/SpriteFoldingExtension/Runtime/AutoSpriteFolding.cs
+++ Assets/AutoEdge2Slice/SpriteFoldingExtension/Runtime/AutoSpriteFolding.cs
@@ -26,26 +26,22 @@ namespace AutoEdge2Slice.SpriteFoldingExtension.Runtime
             var sprite = _spriteRenderer.sprite;
             if (ModifiedSprites.Contains(sprite)) return;
             
-            ModifyTexCoord(sprite);
-            ModifyVertex(sprite);
+            
+            //そのままだと、頂点の座標のxy座標がUV座標として利用されてしまうため上書きする
+            var texCoord = sprite.GetVertexAttribute<Vector2>(VertexAttribute.TexCoord0);
+            var texCoordArray = new NativeArray<Vector2>(texCoord.ToArray(), Allocator.Temp);
+            sprite.SetVertexAttribute(VertexAttribute.TexCoord0, texCoordArray);
+            
+            //頂点座標をpivotにあわせて曲げる処理
+            var vertexPosition = sprite.GetVertexAttribute<Vector3>(VertexAttribute.Position);
+            var modifiedPosition = CalculateVerticesToBendSprite(vertexPosition);
+            sprite.SetVertexAttribute(VertexAttribute.Position,modifiedPosition);
+
+            
             ModifiedSprites.Add(sprite);
         }
 
-        private void ModifyTexCoord(Sprite sprite)
-        {
-            var texCoord = sprite.GetVertexAttribute<Vector2>(VertexAttribute.TexCoord0);
-            var texCoordArray = new NativeArray<Vector2>(texCoord.ToArray(), Allocator.Temp);
-            sprite.SetVertexAttribute<Vector2>(VertexAttribute.TexCoord0, texCoordArray);
-        }
-
-        private void ModifyVertex(Sprite sprite)
-        {
-            var s = sprite.GetVertexAttribute<Vector3>(VertexAttribute.Position);
-            var set = ConvertVertex(s);
-            sprite.SetVertexAttribute<Vector3>(VertexAttribute.Position,set);
-        }
-
-        private NativeArray<Vector3> ConvertVertex(NativeSlice<Vector3> native)
+        private NativeArray<Vector3> CalculateVerticesToBendSprite(NativeSlice<Vector3> native)
         {
             var retValue = new NativeArray<Vector3>(native.Length,Allocator.Temp);
             for (var i = 0; i < native.Length; i++)
